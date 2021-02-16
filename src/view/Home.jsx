ff6a56702456e2882b4a66b2c5c21c4c3c58cacb
Houra <hu.a.mashaee@gmail.com>
2021-02-16 15:33:40 +0100

    photos done

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,51 +1,49 @@
 import React from "react";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import CityCard from "../components/CityCard";
-import getHomeData from "../Utils/Api"
+import getHomeData from "../utils/Api";
 
 class Home extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            cities: [],
+  constructor() {
+    super();
+    this.state = {
+      cities: [],
+    };
+  }
 
-        };
-    }
+  componentDidMount() {
+    getHomeData().then((response) => {
+      this.setState({
+        cities: response.cities,
+        image: response.source,
+      });
+      console.log("cities", this.state.cities);
+      console.log("image", this.state.cities);
+    });
+  }
 
-
-    componentDidMount() {
-        getHomeData()
-            .then(response => {
-                this.setState({
-                    cities: response.cities,
-                    image: response.source
-                })
-                console.log("cities", this.state.cities)
-                console.log("image", this.state.cities)
-            })
-    }
-
-
-    render() {
-        return (
-            <div className="container d-flex flex-column justify-content-center">
-                <h1 className="text-center tabTitle">Popular</h1>
-                <div className="row">
-                    {this.state.cities.map((city) => {
-                        return (
-            
-                
-            );
-                    })}
+  render() {
+    return (
+      <div className='container d-flex flex-column justify-content-center'>
+        <h1 className='text-center tabTitle'>Home</h1>
+        <div className='row'>
+          {this.state.cities.map((city) => {
+            return (
+              <div className='filmCard mb-3'>
+                <img
+                  src={`http://localhost:3002/img/${city.slug}.png`}
+                  className='card-img-top'
+                  alt={`${city.name}`}
+                />
+                <div className='card-body'>
+                  <h4 className='card-title'>{city.name}</h4>
                 </div>
-            </div >
-
-
-
-
-
-        )
-    }
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
