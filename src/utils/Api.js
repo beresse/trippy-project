06e4dd0ffa76b1b4d651d720b1acbeac271f82e1
el.beress@gmail.com <el.beressa@gmail.com>
2021-02-16 15:22:35 +0100

    git pour Houra

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 const config = {
   host: "http://localhost:3002",
-};
+};// un commentaire pour Houra
 const getHomeData = () => {
   return fetch("http://localhost:3002/api/home").then((response) =>
     response.json()
